1bbe66ab721494a5030523a10d5967fb0647960f
stéphane BERTHO <stephane.bertho@laposte.fr>
2018-05-13 23:13:02 +0200

    style(theme): adding theme and adjustements

--- src/pages/tab2/tab2.ts
+++ src/pages/tab2/tab2.ts
@@ -8,7 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Tab2Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private cal) {
   }
-
 }
